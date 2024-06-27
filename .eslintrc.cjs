9b3a7c87b384dcf50d7d2eebcd19a5d8f7997e95
Sokilskill <vprovorotov@gmail.com>
2024-06-27 15:18:48 +0300

    added new rules for lint-staged as no-console

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,19 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "no-console": "error",
+    "no-warning-comments": ["warn", { terms: [], location: "anywhere" }],
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
