3b89f8c9145e47db02ae54f7d92379103c9fe70a
Sokilskill <vprovorotov@gmail.com>
2024-06-29 19:59:01 +0300

    create LogoIcon

---
+++ src/components/Icons/LogoIcon.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { Icon, IconProps } from "@chakra-ui/react";
+
+export const LogoIcon: FC<IconProps> = (props) => (
+  <Icon viewBox="0 0 28 28" boxSize="28px" {...props}>
+    <path
+      d="M14 28C21.732 28 28 21.732 28 14C28 6.26801 21.732 0 14 0C6.26801 0 0 6.26801 0 14C0 21.732 6.26801 28 14 28Z"
+      fill="#FFDA44"
+    />
+    <path
+      d="M0 14C0 6.26806 6.26806 0 14 0C21.7319 0 28 6.26806 28 14"
+      fill="#338AF3"
+    />
+  </Icon>
+);
