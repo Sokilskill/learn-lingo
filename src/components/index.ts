3e46e876bb533be0e105f3713c46f2224a18742c
Sokilskill <vprovorotov@gmail.com>
2024-07-03 10:21:38 +0300

    feat/ update import
    -додав файл index.ts зякого отрумую всі імпорти

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import { SharedLayout } from "./SharedLayout/SharedLayout";
+import { Greeting } from "./Greeting/Greeting";
+import { StatisticsList } from "./StatisticsList/StatisticsList";
+import { TeacherCard } from "./TeacherCard/TeacherCard";
+
+export { SharedLayout, Greeting, StatisticsList, TeacherCard };
---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+import HomePage from "./HomePage/HomePage";
+import TeachersPage from "./TeachersPage/TeachersPage";
+import FavoritesPage from "./FavoritePage/FavoritesPage";
+
+export { HomePage, TeachersPage, FavoritesPage };
